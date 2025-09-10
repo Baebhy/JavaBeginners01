091172aaf95964c6207bdd2294c039cb396a9083
GeneraL <GeneraL@192.168.1.174>
2025-09-10 04:50:41 +0100

    thrid commit

--- JavaBeginners01/src/defaultt/HelloWorld.java
+++ JavaBeginners01/src/defaultt/HelloWorld.java
@@ -16,6 +16,7 @@ public class HelloWorld {
 		System.out.println("Variable is something whose value can change, constant is something whose value remains constant until it is changed Dataype says what kind of data you are storing and how much memory");
 		System.out.println("String=alphanumeric, int=number, float=decimal number, boolean=true or false double=large decimal, char=single quote");
 		System.out.println("Keywords/reserved words are kept by the language for itself and should not be used as a variable name");
+		System.out.println("Save test");
 		
 	}
 
