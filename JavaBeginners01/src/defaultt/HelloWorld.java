9dbf4a2f59eb0f898251975009c695298ddb52c2
GeneraL <GeneraL@192.168.1.174>
2025-09-10 04:41:08 +0100

    First Commit

---
+++ JavaBeginners01/src/defaultt/HelloWorld.java
@@ -0,0 +1,22 @@
+package defaultt;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.print("This is how we print in Java");
+		System.out.println("Java is fun!");
+		System.out.println("Favour Adedokun");
+		System.out.println("Student an005");
+		System.out.println("favouradedokun@testemail.com");
+		System.out.println("1");
+		System.out.println(" 2");
+		System.out.println("  3");
+		System.out.println("   4");
+		System.out.println("    5");
+		System.out.println("Variable is something whose value can change, constant is something whose value remains constant until it is changed Dataype says what kind of data you are storing and how much memory");
+		System.out.println("String=alphanumeric, int=number, float=decimal number, boolean=true or false double=large decimal, char=single quote");
+		System.out.println("Keywords/reserved words are kept by the language for itself and should not be used as a variable name");
+		
+	}
+
+}
