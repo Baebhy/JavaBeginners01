99a4562bf9f6bc368d149bba8dfefe235352579f
GeneraL <GeneraL@genuine-software.autodesk.com>
2025-09-12 17:33:51 +0100

    Pound and euro converter feature added

--- JavaBeginners01/src/defaultt/CurrencyConverter.java
+++ JavaBeginners01/src/defaultt/CurrencyConverter.java
@@ -6,11 +6,16 @@ public class CurrencyConverter {
 		double dollarrate=1500.56;
 		double poundrate=1729.44;
 		double eurorate=2188.84;
-		double dollaramount=452.62;
+		double dollaramount=457.62;
 		double poundamount=683.28;
 		double euroamount=888.33;
 		double dollartonaira=dollaramount*dollarrate;
-		System.out.println("452.62 dollars to naira is "+ dollartonaira);
+		System.out.println(dollaramount +" dollars to naira is " + dollartonaira);
+		double poundtonaira=poundamount*poundrate;
+		System.out.println(poundamount + " pounds to naira is " + poundtonaira);
+		double eurotonaira=euroamount * eurorate;
+		System.out.println(euroamount + " euros to naira is " + eurotonaira);
+		
 		
 
 	}
