c40bb1bb0f59fbb5bf6e9d9f4a59bace090f8fc5
GeneraL <GeneraL@genuine-software.autodesk.com>
2025-09-12 17:18:55 +0100

    Currency Converter

---
+++ JavaBeginners01/src/defaultt/CurrencyConverter.java
@@ -0,0 +1,18 @@
+package defaultt;
+
+public class CurrencyConverter {
+
+	public static void main(String[] args) {
+		double dollarrate=1500.56;
+		double poundrate=1729.44;
+		double eurorate=2188.84;
+		double dollaramount=452.62;
+		double poundamount=683.28;
+		double euroamount=888.33;
+		double dollartonaira=dollaramount*dollarrate;
+		System.out.println("452.62 dollars to naira is "+ dollartonaira);
+		
+
+	}
+
+}
