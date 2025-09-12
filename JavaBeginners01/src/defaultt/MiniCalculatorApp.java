2fbb025eb8461707d224db719ba7e731077cb40f
GeneraL <GeneraL@192.168.1.174>
2025-09-12 06:55:16 +0100

    Mini Calculator App

---
+++ JavaBeginners01/src/defaultt/MiniCalculatorApp.java
@@ -0,0 +1,13 @@
+package defaultt;
+
+public class MiniCalculatorApp {
+
+	public static void main(String[] args) {
+		int FirstNumber=40;
+		int SecondNumber=80;
+		int sum=FirstNumber+SecondNumber;
+		System.out.println("The sum of the two numbers is "+sum);
+
+	}
+
+}
