beead18076e4b96a5a7d4448002326564e7c9832
GeneraL <GeneraL@192.168.1.174>
2025-09-12 07:23:33 +0100

    Update to Age calculator

--- JavaBeginners01/src/defaultt/AgeCalculator.java
+++ JavaBeginners01/src/defaultt/AgeCalculator.java
@@ -5,7 +5,7 @@ public class AgeCalculator {
 	public static void main(String[] args) {
 		int YearofBirth=1954;
 		int AgeIn2025=2025-YearofBirth;
-		System.out.println("You are " +AgeIn2025+" years old");
+		System.out.println("You are " + AgeIn2025 +" years old");
 
 	}
 
