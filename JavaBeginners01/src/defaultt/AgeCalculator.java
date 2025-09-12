3ad5a7f7b77b2f5cf6bc745d2968e37d0884b34e
GeneraL <GeneraL@192.168.1.174>
2025-09-12 07:22:14 +0100

    Age Calculaor

---
+++ JavaBeginners01/src/defaultt/AgeCalculator.java
@@ -0,0 +1,12 @@
+package defaultt;
+
+public class AgeCalculator {
+
+	public static void main(String[] args) {
+		int YearofBirth=1954;
+		int AgeIn2025=2025-YearofBirth;
+		System.out.println("You are " +AgeIn2025+" years old");
+
+	}
+
+}
