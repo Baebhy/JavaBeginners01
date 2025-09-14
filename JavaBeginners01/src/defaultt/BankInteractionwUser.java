494e03aaa7b0026ad413e256071cc29cb6f38265
GeneraL <favourgbenga18@gmail.com>
2025-09-14 17:56:50 +0100

    Account number feature added

--- JavaBeginners01/src/defaultt/BankInteractionwUser.java
+++ JavaBeginners01/src/defaultt/BankInteractionwUser.java
@@ -10,8 +10,11 @@ public class BankInteractionwUser {
 		System.out.println("Please enter your name");
 		name=KeyBoard.nextLine();
 		System.out.println(name + ", Welcome to Pager Group ATM");
+		int Aza;
+		System.out.println("Please enter destination 10-digit account number");
+		Aza=KeyBoard.nextInt();
+		System.out.println(name+", you entered " + Aza);
 		
-
 	}
 
 }
