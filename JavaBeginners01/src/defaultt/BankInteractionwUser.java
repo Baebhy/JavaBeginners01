933214be4f2d56493159a01a6709f340931d9d6d
GeneraL <favourgbenga18@gmail.com>
2025-09-14 17:54:36 +0100

    Bank Interaction Sequence

---
+++ JavaBeginners01/src/defaultt/BankInteractionwUser.java
@@ -0,0 +1,17 @@
+package defaultt;
+
+import java.util.Scanner;
+
+public class BankInteractionwUser {
+
+	public static void main(String[] args) {
+		Scanner KeyBoard=new Scanner(System.in);
+		String name;
+		System.out.println("Please enter your name");
+		name=KeyBoard.nextLine();
+		System.out.println(name + ", Welcome to Pager Group ATM");
+		
+
+	}
+
+}
