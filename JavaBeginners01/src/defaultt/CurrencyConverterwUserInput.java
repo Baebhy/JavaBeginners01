38855a29a105c0736a5159466a02d36bd19d86e3
GeneraL <favourgbenga18@gmail.com>
2025-09-15 23:25:09 +0100

    Currency Converter with user Input

---
+++ JavaBeginners01/src/defaultt/CurrencyConverterwUserInput.java
@@ -0,0 +1,16 @@
+package defaultt;
+
+import java.util.Scanner;
+
+public class CurrencyConverterwUserInput {
+
+	public static void main(String[] args) {
+		Scanner KeyBoard=new Scanner(System.in);
+		float amount;
+		System.out.println("Please enter amount to convert");
+		amount=KeyBoard.nextFloat();
+		
+
+	}
+
+}
