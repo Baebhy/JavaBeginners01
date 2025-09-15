99683dbff04e6456375c7378ab6dd7d816af4846
GeneraL <favourgbenga18@gmail.com>
2025-09-16 00:07:03 +0100

    Text corrections

--- JavaBeginners01/src/defaultt/CurrencyConverterwUserInput.java
+++ JavaBeginners01/src/defaultt/CurrencyConverterwUserInput.java
@@ -7,20 +7,20 @@ public class CurrencyConverterwUserInput {
 	public static void main(String[] args) {
 		Scanner KeyBoard=new Scanner(System.in);
 		float dollaramount;
-		System.out.println("Please enter amount in dollars");
+		System.out.println("Please enter amount in Dollars");
 		dollaramount = KeyBoard.nextFloat();
 		float dollarrate = 1500.56f; 
 		float naira = dollarrate * dollaramount;
-		System.out.println(dollaramount + "USD is " + naira + " in naira" );
+		System.out.println(dollaramount + " USD is " + naira + " in Naira" );
 		
 		float da;
-		System.out.println("Enter amount in pounds");
+		System.out.println("Please enter amount in Pounds");
 		da = KeyBoard.nextFloat();
 		float dr;
-		System.out.println("Enter rate");
+		System.out.println("Please enter preferred Pounds rate");
 		dr = KeyBoard.nextFloat();
 		float na = da * dr;
-		System.out.println(da + " pounds is " + na + " in naira" );
+		System.out.println(da + " Pounds is " + na + " in Naira" );
 		
 
 	}
