3d01f1aa463e076b91779e3bb9b4974aeb991c9d
GeneraL <favourgbenga18@gmail.com>
2025-09-15 23:55:48 +0100

    Updated for Dollar to Naira

--- JavaBeginners01/src/defaultt/CurrencyConverterwUserInput.java
+++ JavaBeginners01/src/defaultt/CurrencyConverterwUserInput.java
@@ -6,9 +6,12 @@ public class CurrencyConverterwUserInput {
 
 	public static void main(String[] args) {
 		Scanner KeyBoard=new Scanner(System.in);
-		float amount;
-		System.out.println("Please enter amount to convert");
-		amount=KeyBoard.nextFloat();
+		float dollaramount;
+		System.out.println("Please enter amount in dollars");
+		dollaramount = KeyBoard.nextFloat();
+		float dollarrate = 1500.56f; 
+		float naira = dollarrate * dollaramount;
+		System.out.println(dollaramount + "USD is " + naira + " in naira" );
 		
 
 	}
